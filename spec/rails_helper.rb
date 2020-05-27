c63dad825b6a0fd7df025cbe9b86075f3ead8442
Tyler Porter <tylerpporter@gmail.com>
2020-05-27 10:49:59 -0600

    Disable vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,9 +67,9 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  # config.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
-  config.configure_rspec_metadata!
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+#   config.hook_into :webmock
+#   # config.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
+#   config.configure_rspec_metadata!
+# end
