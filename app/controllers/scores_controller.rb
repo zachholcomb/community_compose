1bfc11e719f6467d6142d10c57e25e02a4559813
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-27 21:48:54 -0600

    Fixes Rubocop violations

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,8 +1,6 @@
 class ScoresController < ApplicationController
-
-    def index
-      # @score = FlatService.get_score(params[:score_id])
-      @score = Score.show(params[:score_id])
-    end
-
+  def index
+    # @score = FlatService.get_score(params[:score_id])
+    @score = Score.show(params[:score_id])
+  end
 end
