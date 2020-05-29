ea41252d8b320fe14fb18b4955ce836e2a8e8f77
Andrew Reid <areid.sj@gmail.com>
2020-05-29 14:33:41 -0600

    Clean up test before each block

--- spec/features/collaboration/collab_show_spec.rb
+++ spec/features/collaboration/collab_show_spec.rb
@@ -8,19 +8,22 @@ RSpec.describe 'Collaborators Features: ', type: :feature do
       allow_any_instance_of(ApplicationController).to receive(:current_user)
                                                   .and_return(@logged_in_user)
       json_user_resp = File.read('spec/fixtures/flat/user.json')
-      stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200, body: json_user_resp, headers: {})
+      stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200,
+                                                                body: json_user_resp,
+                                                                headers: {})
       json_score_resp = File.read('spec/fixtures/flat/user_scores.json')
-      stub_request(:get, "https://api.flat.io/v2/users/me/scores").to_return(status: 200, body: json_score_resp, headers: {})
-
-      visit users_dashboard_index_path
-
+      stub_request(:get, "https://api.flat.io/v2/users/me/scores").to_return(status: 200,
+                                                                             body: json_score_resp,
+                                                                             headers: {})
       json_score_show_resp = File.read('spec/fixtures/flat/score_show.json')
       expected = nil
       File.open('spec/fixtures/flat/user_scores.json') do |file|
         file.each_line { |line| expected = JSON.parse(line, symbolize_names: true) }
       end
-      stub_request(:get, "https://api.flat.io/v2/scores/#{expected[0][:id]}").to_return(status: 200, body: json_score_show_resp, headers: {})
-
+      stub_request(:get, "https://api.flat.io/v2/scores/#{expected[0][:id]}").to_return(status: 200,
+                                                                                        body: json_score_show_resp,
+                                                                                        headers: {})                                                        
+      visit users_dashboard_index_path
       within('.scores') do
         click_link 'Funk'
       end
