1a40af3ed64b4c32e7111143b95ab674bd747437
Tyler Porter <tylerpporter@gmail.com>
2020-05-30 17:58:17 -0600

    Update user model spec to account for new validations

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe 'As a registered user' do
     json_score_resp = File.read('spec/fixtures/flat/create_score/users_scores_create_score.json')
     stub_request(:get, "https://api.flat.io/v2/users/me/scores").to_return(status: 200, body: json_score_resp, headers: {})
 
-    User.create!(email: 'zachholcombmusic@gmail.com')
+    create(:user, email: 'zachholcombmusic@gmail.com')
 
     visit '/login'
     fill_in "Email",	with: "zachholcombmusic@gmail.com"
     click_on 'Log In'
 
-    expect(page).to have_current_path('/users/dashboard') 
+    expect(page).to have_current_path('/users/dashboard')
   end
 end
