3ccc18df696c1fb436d4e671438d37684625293e
Tyler Porter <tylerpporter@gmail.com>
2020-05-30 19:29:56 -0600

    Create dashboard link to my profile

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -10,10 +10,14 @@ describe 'As a registered user' do
     end
   end
   describe 'when I visit my dashboard' do
-    xscenario 'I can click a link to view my profile' do
+    scenario 'I can click a link to view my profile' do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user)
                                                   .and_return(user)
+      json_user_resp = File.read('spec/fixtures/flat/user.json')
+      stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200, body: json_user_resp, headers: {})
+      json_score_resp = File.read('spec/fixtures/flat/user_scores.json')
+      stub_request(:get, "https://api.flat.io/v2/users/me/scores").to_return(status: 200, body: json_score_resp, headers: {})
       visit users_dashboard_index_path
       click_link 'My Profile'
 
