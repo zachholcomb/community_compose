4e7cbc586e1d2201ede9d793fc5cb5f497bf805a
Tyler Porter <tylerpporter@gmail.com>
2020-05-31 21:10:28 -0600

    Create user_permitted? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user,
+                :user_permitted?
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_permitted?
+    current_user && current_user.id == params[:id].to_i
   end
 end
