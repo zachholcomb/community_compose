8e1dda2b87ce9f9db975b8fa7b31d62b5b88c0bb
Tyler Porter <tylerpporter@gmail.com>
2020-05-31 21:53:07 -0600

    Remove unused private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def edit
-  #   failure if !user_permitted?
-  #   @user = User.find(params[:id])
-  # end
-  #
-  # def update
-  #   user_id = User.find(params[:id]).id
-  #   User.update(user_id, user_params)
-  #   redirect_to user_path(user_id)
-  # end
-
   private
 
   def user_params
     params.permit(:username)
-  end
-
-  def failure
-    render file: '/public/404'
   end
 end
