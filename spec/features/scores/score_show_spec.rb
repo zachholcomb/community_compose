07d614258b5c2ca46256f4b9ac08de36bc857282
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-03 12:08:11 -0600

    In progress scores dropdown

--- spec/features/scores/score_show_spec.rb
+++ spec/features/scores/score_show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'As a User', type: :feature do
                                                 .and_return(@user)
     json_user_resp = File.read('spec/fixtures/flat/user.json')
     stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200, body: json_user_resp, headers: {})
-    json_score_resp = File.read('spec/fixtures/flat/user_scores.json')
+    json_score_resp = File.read('spec/fixtures/flat/scores_list.json')
     stub_request(:get, "https://api.flat.io/v2/users/me/scores").to_return(status: 200, body: json_score_resp, headers: {})
 
     visit users_dashboard_index_path
@@ -18,14 +18,14 @@ RSpec.describe 'As a User', type: :feature do
   it 'I can see a score show page' do
     json_score_resp = File.read('spec/fixtures/flat/score_show.json')
     
-    expected = nil
-    File.open('spec/fixtures/flat/user_scores.json') do |file|
-      file.each_line do |line|
-        expected = JSON.parse(line, symbolize_names: true)
-      end
-    end
+    # expected = nil
+    # File.open('spec/fixtures/flat/user_scores.json') do |file|
+    #   file.each_line do |line|
+    #     expected = JSON.parse(line, symbolize_names: true)
+    #   end
+    # end
     
-    stub_request(:get, "https://api.flat.io/v2/scores/#{expected[0][:id]}").to_return(status: 200, body: json_score_resp, headers: {})
+    stub_request(:get, "https://api.flat.io/v2/scores/5ed093f4a892cd59c611e0fc").to_return(status: 200, body: json_score_resp, headers: {})
     
     
     within('.scores') do
@@ -33,30 +33,32 @@ RSpec.describe 'As a User', type: :feature do
     end
     
     expect(current_path).to eq(scores_path)
-    expect(page).to have_content(expected[0][:title])
+    expect(page).to have_content('Funk')
     expect(page).to have_css('#embed-container')
   end
 
   it 'I can select a score from the dropdown menu' do
     json_score_resp = File.read('spec/fixtures/flat/score_show.json')
     
-    expected = nil
-    File.open('spec/fixtures/flat/user_scores.json') do |file|
-      file.each_line do |line|
-        expected = JSON.parse(line, symbolize_names: true)
-      end
-    end
+    # expected = nil
+    # File.open('spec/fixtures/flat/user_scores.json') do |file|
+    #   file.each_line do |line|
+    #     expected = JSON.parse(line, symbolize_names: true)
+    #   end
+    # end
     
-    stub_request(:get, "https://api.flat.io/v2/scores/#{expected[0][:id]}").to_return(status: 200, body: json_score_resp, headers: {})
+    stub_request(:get, "https://api.flat.io/v2/scores/5ed093f4a892cd59c611e0fc").to_return(status: 200, body: json_score_resp, headers: {})
     
     
     within('.scores') do
       click_link 'Funk'
     end
     
-    select 'Country', from: 'score-menu'
-
-    expect(page).to have_content('Country')
-    expect(page).not_to have_content('Funk')
+    select 'Country', from: 'scores-menu'
+    
+    within('#score-title') do
+      expect(page).to have_content('Country')
+      expect(page).not_to have_content('Funk')
+    end
   end
 end
