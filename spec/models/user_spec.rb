78c8c298429f3e09d19c1114a55ef9a5ec4b862a
Tyler Porter <tylerpporter@gmail.com>
2020-06-04 15:33:53 -0600

    Create instance method tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:interests) }
     it { should validate_presence_of(:instruments) }
   end
+
+  describe 'instance_methods' do
+    it 'picture' do
+      user = create(:user, username: 'tylerpporter', zip: '80005')
+      json_user_resp = File.read('spec/fixtures/flat/user.json')
+      stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200, body: json_user_resp, headers: {})
+
+      expect(user.picture).to eq("https://lh3.googleusercontent.com/a-/AOh14GgflwGrAlrgssFjPx_xfrIu9Z4-GpvA1B5n60QU")
+    end
+    it 'distance' do
+      user = create(:user, username: 'tylerpporter', zip: '80005')
+      json_user_resp = File.read('spec/fixtures/flat/user.json')
+      stub_request(:get, "https://api.flat.io/v2/me").to_return(status: 200, body: json_user_resp, headers: {})
+      zips = {"80004" => 1.4, "80005" => 2.3, "80006" => 4.2}
+
+      expect(user.distance(zips)).to eq(2.3)
+    end
+  end
 end
