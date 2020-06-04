e31b75f13ff1da7415867420d66130b5990d936c
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-04 15:59:50 -0600

    Add user model methods for mailboxer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  acts_as_messageable
   validates :email, presence: true
   validates :zip, presence: true
   validates :flat_id, presence: true
@@ -6,7 +7,16 @@ class User < ApplicationRecord
   validates :interests, presence: true
   validates :instruments, presence: true
 
+
   def distance(distance_list)
     distance_list[zip]
+  end
+
+  def mailboxer_email(object)
+    return self.email
+  end
+
+  def name
+    return self.username
   end
 end
