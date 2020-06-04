8d9f904836261942a382002c276731568b2f774c
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-04 15:59:09 -0600

    Add conversations controller

---
+++ app/controllers/users/conversations_controller.rb
@@ -0,0 +1,25 @@
+class Users::ConversationsController < ApplicationController
+  def index
+    @conversations = current_user.mailbox.conversations
+  end
+
+  def show
+    @conversation = current_user.mailbox.conversations.find(params[:id])
+  end
+
+  def new
+    @recipients = User.where.not(id: current_user.id)
+  end
+
+  def create 
+    recipient = User.find(params[:user_id])
+    receipt = current_user.send_message(recipient, params[:body], params[:subject])
+    redirect_to users_conversation_path(receipt.conversation)
+  end
+
+  def destroy 
+    conversation = current_user.mailbox.conversations.find(params[:id])
+    conversation.move_to_trash(current_user)
+    redirect_to users_conversations_path
+  end
+end
