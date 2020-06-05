1f7c33573b1e442d68c0ca455a2662b679f789d6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-05 01:24:41 -0600

    Update edit test

--- spec/features/scores/score_edit_spec.rb
+++ spec/features/scores/score_edit_spec.rb
@@ -14,22 +14,4 @@ RSpec.describe 'As a registered user' do
 
     visit users_dashboard_index_path
   end
-
-  it 'I can edit scores' do
-    score_id = '5ed093f4a892cd59c611e0fc'
-
-    json_score_resp = File.read('spec/fixtures/flat/create_score/create_new_score.json')
-    stub_request(:get, "https://api.flat.io/v2/scores/#{score_id}").to_return(status: 200, body: json_score_resp, headers: {})
-
-
-    within '.scores' do
-      within '#score-0' do
-        click_on('My New Score')
-      end
-    end
-
-    click_on('Submit Changes')
-
-    expect(page).to have_current_path(users_dashboard_index_path)
-  end
 end
