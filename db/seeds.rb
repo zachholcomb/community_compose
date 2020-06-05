3cfa897c02a364c21fd7264fe790171331491926
Andrew Reid <areid.sj@gmail.com>
2020-06-04 19:38:21 -0600

    Update background image display

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-
-User.create(email: 'user_one@example.com',
-            zip: '80218',
-            username:'areid.sj',
-            flat_id: '5ecda42869109757b671ceaa',
-            about: 'I like music',
-            interests: 'Music',
-            instruments: 'Guitar')
